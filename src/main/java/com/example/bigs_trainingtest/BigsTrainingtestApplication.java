f215eb8223e18683f5faa6f4bfa46d1c8a0da085
sanghoon1109 <tkdgns7108@email.com>
2024-04-11 15:12:55 +0900

    init commit

---
+++ src/main/java/com/example/bigs_trainingtest/BigsTrainingtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigs_trainingtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigsTrainingtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BigsTrainingtestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bigs_trainingtest/BigsTrainingtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigs_trainingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigsTrainingtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
